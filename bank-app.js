dc7880c4438884df7f7f9c7788107e139dc3bca7
Vive Velasco <48145286+DarkyRobin@users.noreply.github.com>
2024-04-09 10:21:36 +0800

    Fix error message

--- bank-app.js
+++ bank-app.js
@@ -4,7 +4,7 @@ class BankAccount {
     this.balance = initialDeposit;
   }
 
-  deposit(amount){
+  deposit(amount) {
     if (amount < 0) {
       throw new Error("Deposit amount should be greater than 0.");
     }
@@ -14,28 +14,28 @@ class BankAccount {
   withdraw(amount) {
     if (amount < 0) {
       throw new Error("Withdrawal amount should be greater than 0.");
-    } 
+    }
 
-    if (this.balance < amount ) {
+    if (this.balance < amount) {
       throw new Error("Insufficient funds");
     }
 
     this.balance -= amount;
   }
 
-  getBalance(){
+  getBalance() {
     return this.balance;
   }
 }
 
 class BankService {
-  constructor(){
+  constructor() {
     this.accounts = {};
   }
   //account creation
   createAccount(initialDeposit) {
     if (initialDeposit < 0) {
-      throw new Error("Initial Deposit amount should be greater than 0.")
+      throw new Error("Initial Deposit amount should be greater than 0.");
     }
 
     const acctID = Object.keys(this.accounts).length + 1;
@@ -44,29 +44,29 @@ class BankService {
   }
 
   //deposit to account
-  depositToAccount(acctID, amount){
+  depositToAccount(acctID, amount) {
     this.getAccount(acctID).deposit(amount);
   }
 
   //withdraw from account
-  withdrawFromAccount(acctID, amount){
+  withdrawFromAccount(acctID, amount) {
     this.getAccount(acctID).withdraw(amount);
   }
 
   //tranfer fund between 2 accounts
-  transferBetweenAccount(fromAcctID, toAcctID, amount){
+  transferBetweenAccount(fromAcctID, toAcctID, amount) {
     const fromAccountID = this.getAccount(fromAcctID);
     const toAccounttID = this.getAccount(toAcctID);
 
     if (fromAccountID.getBalance() < amount) {
-      throw new Error("Insufficient funds to transfer.")
+      throw new Error("Insufficient funds to transfer.");
     }
 
     fromAccountID.withdraw(amount);
     toAccounttID.deposit(amount);
   }
 
-  getAccountBalance(acctID){
+  getAccountBalance(acctID) {
     return this.getAccount(acctID).getBalance();
   }
 
@@ -74,12 +74,11 @@ class BankService {
     const account = this.accounts[accountId];
 
     if (!account) {
-        throw new Error(`Account ${accountId} does not exist.`);
+      throw new Error(`Account ${accountId} does not exist.`);
     }
     return account;
+  }
 }
-}
-
 
 /**
  * TEST SCENARIOS
@@ -87,42 +86,51 @@ class BankService {
 
 function createAccountTest() {
   const account = new BankAccount(1, 100);
-  console.assert(account.getBalance() === 100, 'Failed to initialize account with correct balance');
+  console.assert(
+    account.getBalance() === 100,
+    "Failed to initialize account with correct balance"
+  );
 }
 
 function depositTest() {
   const account = new BankAccount(2, 100);
   account.deposit(50);
-  console.assert(account.getBalance() === 150, 'Failed to deposit');
+  console.assert(account.getBalance() === 150, "Failed to deposit");
 }
 
 function withdrawTest() {
   const account = new BankAccount(2, 150);
   account.withdraw(30);
-  console.assert(account.getBalance() === 120, 'Failed to withdraw')
-  
+  console.assert(account.getBalance() === 120, "Failed to withdraw");
 }
 
 function overdraftTest() {
   const account = new BankAccount(4, 100);
   try {
     account.withdraw(150);
-    console.error("Failed top throw error on overdraft");
+    console.error("Failed to throw error on overdraft");
   } catch (e) {
-    console.assert(e.message === 'Insufficient funds.', 'Failed to handle overdraft correctly'); 
+    console.assert(
+      e.message === "Insufficient funds",
+      "Failed to handle overdraft correctly"
+    );
   }
+
 }
+
 
 function invalidDepositTest() {
-  const account = new BankAccount(.4, 100);
+  const account = new BankAccount(0.4, 100);
   try {
     account.deposit(-50);
-    console.error('Failed to throw error on invalid deposit amount');
+    console.error("Failed to throw error on invalid deposit amount");
   } catch (e) {
-    console.assert(e.message === 'Deposit amount must be positive.', 'Failed to handle invalid deposit amount correctly');
+    console.assert(
+      e.message === "Deposit amount should be greater than 0.",
+      "Failed to handle invalid deposit amount correctly"
+    );
+  }
 }
-}
-
 
 // Running the tests
 function runTests() {
@@ -134,6 +142,5 @@ function runTests() {
 
   console.log("All tests completed.");
 }
-
 
 runTests();
